06139dbd7baad503f75feb5505125cf294af916d
[Wijkarn] <[mathias.wijk@hotmail.com]>
2023-05-21 15:46:19 +0200

    Css

--- src/components/CrewMember.jsx
+++ src/components/CrewMember.jsx
@@ -2,8 +2,8 @@ export default function CrewMember({name, imgSrc}){
 
     return(
         <div className="crew-member">
-            <h3 className="crew-member-name">{name}</h3>
-            <img src={imgSrc} className="crew-member"/>
+            <h2 className="crew-member-name">{name}</h2>
+            <img src={imgSrc} className="crew-member-img"/>
         </div>
     );
 }
